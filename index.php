1ef57a370ef07a0bbfce17e2fd350de221627f1d
Steven Harper <shwesh279@gmail.com>
2018-03-23 12:18:02 -0700

    added through command line

---
+++ index.php
@@ -0,0 +1,10 @@
+<link href="css/sjh.css" rel="stylesheet">
+
+	<section id="list" class="list-section">
+      <div class="container">
+        <div class="col-lg-8 mx-auto">
+          <h2>Computers:<h2>
+          <p>hi</p>
+        </div>
+      </div>
+    </section>
